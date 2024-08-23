6ce4d08cb9dbbbaf8ef09180675b20f74cfcae65
Toni Tralice <atralice@gmail.com>
2024-08-22 23:48:42 -0300

    updates landing

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -1,8 +1,10 @@
+import logo from '../../assets/logo.png'
+
 function Logo({ className }: { className?: string }) {
   return (
     <img
       className={`h-12 w-12 rounded-lg ${className}`} // Tailwind classes for size and border-radius
-      src="./assets/logo.png"
+      src={logo} // Imported image reference
       alt="Company Logo"
     />
   )
